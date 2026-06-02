54a187ffc2ac8de180a25b83a64e3d054a56d0a8
michaelilab <102897547+michaelilab@users.noreply.github.com>
2026-06-02 11:53:31 +0300

    Update 01_build_smalt_index.sh

--- Leishmania_polyA_RNAseq_analysis/scripts/01_build_smalt_index.sh
+++ Leishmania_polyA_RNAseq_analysis/scripts/01_build_smalt_index.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+set -euo pipefail
 
+GENOME="data/reference/Lmajor_Friedlin_v9_genome.fa"
+INDEX_PREFIX="data/reference/Lmajor_Friedlin_v9_smalt"
+
+smalt index "$INDEX_PREFIX" "$GENOME"
